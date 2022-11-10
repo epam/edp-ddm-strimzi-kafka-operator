5822f09108508784cb1b0aac7287d7fdb7ac1c01
Dmytro Zinchenko <dmytro_zinchenko1@epam.com>
2022-11-10 15:02:18 +0200

    [EDP-DDM-18824] Fix: Add ability to set kafka volume size via values

---
+++ upgrade-scripts/pre-upgrade/common/patch-volume-size.groovy
@@ -0,0 +1,17 @@
+void call() {
+    String valuesFile = '/tmp/platform-values.yaml'
+    sh(script: "helm get values registry-configuration -n ${NAMESPACE} > ${valuesFile}")
+    LinkedHashMap platformValues = readYaml file: valuesFile
+    ['kafka', 'zookeeper'].each {
+        String storageSize = platformValues['global']['kafkaOperator']['storage']["${it}"]['size']
+        ArrayList<String> volumes = sh(script: "oc get pvc -l strimzi.io/name=kafka-cluster-${it} --no-headers " +
+                "-o custom-columns=NAME:.metadata.name -n ${NAMESPACE}",
+                returnStdout: true).tokenize('\n')
+        volumes.each { volume ->
+            sh("oc patch pvc ${volume} --type merge -n ${NAMESPACE} " +
+                    "-p '{\"spec\":{\"resources\":{\"requests\":{\"storage\":\"${storageSize}\"}}}}'")
+        }
+    }
+}
+
+return this;
